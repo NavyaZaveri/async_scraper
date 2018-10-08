ca416f63245f3f01a74963f0d915cef98c75a973
NavyaZaveri <navyajayesh+1@gmail.com>
2018-10-08 23:47:57 +0100

    major api change: instead of fetching json, fetch bytes

--- stuff/thing.go
+++ stuff/thing.go
@@ -1,7 +1,10 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"sync"
 )
 
@@ -10,11 +13,13 @@ worker pool implementation
 so you have workers and jobs
 each worker is assigned  some job, concurrently
 
+CHANGE OF PLANS
+do **not** unmarshal, just send the body
+let the user unmarshal
 
 ideal api
-w worker(2).fetch()
 
-new WorkedPool().setJob().fetch()
+NeWorkePool().setJob().fetch()
 
 */
 
@@ -27,24 +32,32 @@ type Job string
 
 type WorkerPool struct {
 	workers []*Worker
-	result  chan string
+	result  chan []byte
 }
 
-func (w *WorkerPool) fetch(p PageIterator) []string {
+func (w *WorkerPool) fetch(p PageIterator) [][]byte {
 	jobs := make(Jobs, 0)
 	w.spawnWorkers(jobs)
 	wg := sync.WaitGroup{}
 	mux := sync.Mutex{}
-	results := []string{}
+	results := [][]byte{}
 	for p.HasNext() {
 		currentPage := p.Next()
 		wg.Add(1)
+
+		//assign a job to  a
+		//worker asynchronously
 		go func(cur string) {
 			defer wg.Done()
+			fmt.Println("CUR IS" + cur)
 			jobs <- Job(cur)
-			mux.Lock()
+
+			//unblocks
+
 			x := <-w.result
+			mux.Lock()
 			results = append(results, x)
+
 			mux.Unlock()
 		}(currentPage)
 	}
@@ -58,8 +71,18 @@ func (w *WorkerPool) spawnWorkers(j Jobs) {
 		go work(worker, j, w.result)
 	}
 }
-func (w *Worker) execute(j Job) string {
-	return string(j)
+
+func (w *Worker) execute(j Job) []byte {
+	url := string(j)
+	fmt.Println(url)
+	resp, err := http.Get(url)
+	body, err := ioutil.ReadAll(resp.Body)
+
+
+	if err == nil {
+		return body
+	}
+	return nil
 
 }
 
@@ -68,27 +91,38 @@ func newWorker() *Worker {
 }
 
 func NewWorkerPool(num int) *WorkerPool {
-	w := &WorkerPool{result: make(chan string, 0)}
+	w := &WorkerPool{result: make(chan []byte, 0)}
 	for i := 0; i < num; i++ {
 		w.workers = append(w.workers, newWorker())
 	}
-
 	return w
 }
 
-func work(w *Worker, jobs Jobs, res chan<- string) {
+func work(w *Worker, jobs Jobs, res chan<- []byte) {
 	for job := range jobs {
+		fmt.Println("job is" + job)
 		ans := w.execute(job)
 
-		//blocks
 		res <- ans
 	}
 }
 
 func main() {
-	b := &blah{}
-	//NewWorkerPool(3).Fetch("HELLO", 10)
-	v := NewWorkerPool(3).fetch(b)
-	fmt.Println(len(v))
-	fmt.Println("done")
-}
+
+	v := NewWorkerPool(20).fetch(&blah{})
+	fmt.Println(v)
+	x := xkcdResp{}
+	sample := v[2]
+	err := json.Unmarshal(sample, &x)
+	if err==nil {
+		fmt.Println(x.Img)
+
+	} else {
+		panic(err)
+	}
+
+
+	}
+
+
+
