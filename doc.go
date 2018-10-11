7695268ac89836b8671e69e3f36f49476fbde991
NavyaZaveri <navyajayesh+1@gmail.com>
2018-10-11 23:08:24 +0100

    add godoc label

--- doc.go
+++ doc.go
@@ -1,4 +1,6 @@
 /*
+# seektar [![GoDoc](https://godoc.org/github.com/NavyaZaveri/seektar?status.svg)](https://godoc.org/github.com/unixpickle/seektar)
+
 
 scraper can be used to asynchronously fetch
 context body from websites.
