224f0e8d38a4041bdb37ef5ee3117b9c8fd9b8c9
NavyaZaveri <navyajayesh+1@gmail.com>
2018-10-11 18:47:56 +0100

    created example dir

---
+++ example/xkcd_example.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/NavyaZaveri/scraper"
+)
+
+/*
+ideal usage?
+NeWorkerPool().fetch()
+
+*/
+
+type JsonResp struct {
+	Img string
+}
+
+func main() {
+
+
+	//spin up twenty workers to fetch stuff from the links
+	//provided by the iterator
+	v := scraper.NewWorkerPool(40).Fetch(&scraper.XkcdIterator{})
+
+	for _,htmlBody_ := range v {
+		js := JsonResp{}
+		err := json.Unmarshal(htmlBody_,&js)
+		if err == nil {
+			fmt.Println(js)
+
+		} else {
+			fmt.Println(err)
+		}
+
+	}
+
+
+}
