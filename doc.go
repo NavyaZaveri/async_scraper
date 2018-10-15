e76719f54e34427a91c12ba5e45cf7ffe15c415e
NavyaZaveri <navyajayesh+1@gmail.com>
2018-10-15 20:52:39 +0100

    add godeps

--- doc.go
+++ doc.go
@@ -5,9 +5,11 @@
 scraper can be used to asynchronously fetch
 context body from websites.
 
-Usage: json_bytes := scraper.NewWorkerPool(100).Fetch(MyPageIterator)
+Usage: json_bytes := scraper.NewWorkerPool(100).Fetch(MyPageIterator
+
 
 */
+
 
 package scraper
 
